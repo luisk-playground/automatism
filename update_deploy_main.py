23995df008f29239f8d1e1ae3d23a8a08e443338
luisk <cabritavazquez@gmail.com>
2024-07-02 10:15:07 +0200

    some files

--- update_deploy_main.py
+++ update_deploy_main.py
@@ -1,7 +1,7 @@
 import yaml
 
 # Leer el archivo deploy-main.yml y extraer los valores de ecr_repository y microservice
-with open('.github/workflows/deploy-main.yml', 'r') as file:
+with open('./.github/workflows/deploy-main.yml', 'r') as file:
     deploy_main = yaml.safe_load(file)
 
 ecr_repository = deploy_main['jobs']['CI-CD-main']['with']['ecr_repository']
@@ -16,7 +16,7 @@ deploy_main_new['jobs']['CI-CD-main']['with']['ecr_repository'] = ecr_repository
 deploy_main_new['jobs']['CI-CD-main']['with']['microservice'] = microservice
 
 # Sobrescribir deploy-main.yml con el contenido actualizado
-with open('.github/workflows/deploy-main.yml', 'w') as file:
+with open('./.github/workflows/deploy-main.yml', 'w') as file:
     yaml.dump(deploy_main_new, file, sort_keys=False)
 
 print("Archivo .github/workflows/deploy-main.yml actualizado con éxito.")
